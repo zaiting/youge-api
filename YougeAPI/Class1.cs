41aaa3e3812f1e0ef71fe1a1a64f615f466f492a
caojp <caojp@authine.com>
2021-03-15 21:32:30 +0800

    基本框架

--- YougeAPI/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace YougeAPI
-{
-    public class Class1
-    {
-    }
-}
---
+++ YougeAPI/Urls.cs
@@ -0,0 +1,15 @@
+﻿namespace YougeAPI
+{
+    public class Urls
+    {
+        /// <summary>
+        /// 生成Token
+        /// </summary>
+        public const string GenerateToken = "";
+
+        /// <summary>
+        /// 获取Token
+        /// </summary>
+        public const string GetToken = "";
+    }
+}
